edb9c1c2d9fb4eed1cdb1f78a480273a79f534ab
yajun.fan <yajun.fan@mljr.com>
2018-11-26 16:21:30 +0800

    地址中文转化

--- src/main/java/com/example/demo/util/unicode/DecodeUnicode.java
+++ src/main/java/com/example/demo/util/unicode/DecodeUnicode.java
@@ -11,22 +11,22 @@ public class DecodeUnicode {
     public static void main(String[] args){
         
         try {
-        	String url="http://image.yyfq.com/econtract/20181121/contract/23403075/dist/纸质退回来_ZX2018112119325312998803043000.pdf";
+        	String url="http://image.yyfq.com/econtract/20181126/contract/23403369/dist/纸质退回来_ZX2018112615375881968703043000.pdf";
             if(StringUtils.isNotEmpty(url) && url.contains(".pdf")){
                 String[] arr = url.split("/");
                 String name=arr[arr.length-1];
                 String[] names = name.split("_");
-                String urlString = URLEncoder.encode(names[0], "GBK");
+                String urlString = URLEncoder.encode(names[0], "utf-8");
                 url=url.replace(names[0],urlString);
                 System.out.println(url);
             }
-            // 将application/x-www-from-urlencoded字符串转换成普通字符串
+            /*// 将application/x-www-from-urlencoded字符串转换成普通字符串
             String keyWord = URLDecoder.decode("%C4%E3%BA%C3", "GBK");
             System.out.println(keyWord);  //输出你好
             // 将普通字符创转换成application/x-www-from-urlencoded字符串
             String urlString = URLEncoder.encode("你好", "GBK");  //输出%C4%E3%BA%C3
 
-            System.out.println(urlString);
+            System.out.println(urlString);*/
         } catch (UnsupportedEncodingException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
