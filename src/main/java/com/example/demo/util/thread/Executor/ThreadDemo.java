3c08d6c0093e96a9bfede1fae7b0b7f22e6749aa
fanyaj@126.com <fanyaj@126.com>
2019-03-05 23:33:17 +0800

    线程池添加方法

--- src/main/java/com/example/demo/util/thread/Executor/ThreadDemo.java
+++ src/main/java/com/example/demo/util/thread/Executor/ThreadDemo.java
@@ -1,22 +1,42 @@
 package com.example.demo.util.thread.Executor;
 
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 public class ThreadDemo {
 
     public static void main(String[] args) {
-    	ExecutorService executorService = Executors.newFixedThreadPool(4);   	
+    	ExecutorService executorService = Executors.newFixedThreadPool(4);   
+    	
+    	executorService.execute(new Runnable() {
+			@Override
+			public void run() {				
+				System.out.println("aaaaa");
+			}    		
+    	});
+    	
     	
     	Future<Object> future = executorService.submit(new Callable<Object>() {
     	        @Override
     	        public Object call() throws Exception {
-    	            throw new RuntimeException("exception in call~");// 该异常会在调用Future.get()时传递给调用者
+    	            //throw new RuntimeException("exception in call~");// 该异常会在调用Future.get()时传递给调用者
+    	        	return "bbbbb";
     	        }
     	    });
     	    
     	try {
     	  Object result = future.get();
+    	  System.out.println(result);
+    	  System.out.println("ccccc");
     	} catch (InterruptedException e) {
     	  // interrupt
     	} catch (ExecutionException e) {
@@ -25,5 +45,23 @@ public class ThreadDemo {
     	}   
     	
     }
+    
+    public void testLatch(ExecutorService executorService, List<Runnable> tasks) throws InterruptedException{
+    	      
+	    CountDownLatch latch = new CountDownLatch(tasks.size());
+	      for(Runnable r : tasks){
+	          executorService.submit(new Runnable() {
+	              @Override
+	              public void run() {
+	                  try{
+	                      r.run();
+	                  }finally {
+	                      latch.countDown();// countDown
+	                  }
+	              }
+	          });
+	      }
+	      latch.await(10, TimeUnit.SECONDS); // 指定超时时间
+	 }
     
 }
